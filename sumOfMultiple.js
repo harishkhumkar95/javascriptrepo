c1d7b0c7ec47682e7c3545e0d5ce183597447a1a
harishkhumkar95 <harishkhumkar95@gmail.com>
2024-09-27 14:14:04 +0100

    new changes

--- sumOfMultiple.js
+++ sumOfMultiple.js
@@ -1,15 +1,13 @@
            
-           const limit =1000;
-            const result = sumofMultiple(limit);
-          function sumofMultiple(limit){
+          
             let sum =0;
-            for(let i=1;i<limit;i++){
+            for(let i=1;i<1000;i++){
                 if(i % 3==0 || i % 5==0){
                     sum +=i;
                 }
             }
-            console.log(sum);
+          
             alert('number is'+sum)
-            console.log(sum);
+            
           
          
