94cbb23a0165837887b8dda074a79e64a16f2d97
harishkhumkar95 <harishkhumkar95@gmail.com>
2024-10-04 19:15:33 +0100

    Create sumOfTwoList.js

---
+++ sumOfTwoList.js
@@ -0,0 +1,20 @@
+function sumOfMultiples(listOfInteger1, listOfInteger2) {
+    let sum = 0;
+
+    for (let num of listOfInteger2) {
+        for (let multiple of listOfInteger1) {
+            if (num % multiple === 0) {
+                sum += num;
+                break; // No need to check other multiples for this num
+            }
+        }
+    }
+
+    return sum;
+}
+
+
+const listOfInteger1 = [2, 3,6,8,5]; 
+const listOfInteger2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+const result = sumOfMultiples(a, l);
+console.log(`The sum of all multiples of elements in a found in l is: ${result}`);
