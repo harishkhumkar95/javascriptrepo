59b81f9d47ee98ba4a5d2000e81a7c3c5287dc28
harishkhumkar95 <harishkhumkar95@gmail.com>
2024-10-04 18:43:58 +0100

    Update sumOfList_a_l.js

--- sumOfList_a_l.js
+++ sumOfList_a_l.js
@@ -10,8 +10,9 @@
 
     const a = [2, 3]; 
     const l = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
-    let sum = 0;
-     function calsum(s,l,sum){
+    
+     function calsum(){
+         let sum = 0;
     alert('working');
     for (let i = 0; i < l.length; i++) {
         for (let j = 0; j < a.length; j++) {
@@ -26,6 +27,7 @@
     console.log(`Sum of multiples: ${sum}`);
     alert('sum of multiple:'+ sum)
      }
+window.onload=calsum;
     
 
 
