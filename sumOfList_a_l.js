c6aa3d4ebd805d32e15f0f079342e209919354c6
harishkhumkar95 <harishkhumkar95@gmail.com>
2024-10-04 18:36:45 +0100

    Update sumOfList_a_l.js

--- sumOfList_a_l.js
+++ sumOfList_a_l.js
@@ -11,7 +11,7 @@
     const a = [2, 3]; 
     const l = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
     let sum = 0;
-     function calsum(){
+     function calsum(s,l,sum){
     alert('working');
     for (let i = 0; i < l.length; i++) {
         for (let j = 0; j < a.length; j++) {
