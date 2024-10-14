ae747330b4d2e6b5413158f0e5930245fd643c81
harishkhumkar95 <harishkhumkar95@gmail.com>
2024-10-14 11:34:37 +0100

    Added input function

--- basket.js
+++ basket.js
@@ -1,26 +1,24 @@
 function calculateTotal(basket,price){
-    let totalCost = 0
-    for(let product in basket)
-    {
-        if(price.hasOwnProperty(product))
-            totalCost = totalCost + (basket[product]*price[product])
+    const basket = {
+        apple: parseInt(document.getElementById('apple').value),
+        mango: parseInt(document.getElementById('mango').value),
+        banana: parseInt(document.getElementById('banana').value)
     }
-    return parseFloat(totalCost.toFixed(2));
-}
-
-const basket ={
-    "apple" : 5,
-    "mango" : 6,
-    "bannana" : 7
-}
-
 
 const price ={
     "apple" : 0.45,
     "mango" : 0.80,
     "bannana" : 0.35
 }
+    let totalCost = 0
+    for(let product in basket)
+    {
+         totalCost = totalCost + (basket[product]*price[product])
+    }
+    return parseFloat(totalCost.toFixed(2));
+}
 
-result = calculateTotal(basket,price)
+
+  result = calculateTotal(basket,price)
 
 alert(result)
