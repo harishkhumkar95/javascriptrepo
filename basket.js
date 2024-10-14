d9b5a7202894508c007f63e92654cce4f69aa7ec
harishkhumkar95 <harishkhumkar95@gmail.com>
2024-10-14 11:50:21 +0100

    push_fix

--- basket.js
+++ basket.js
@@ -1,24 +1,26 @@
-function calculateTotal(basket,price){
-    const basket = {
-        apple: parseInt(document.getElementById('apple').value),
-        mango: parseInt(document.getElementById('mango').value),
-        banana: parseInt(document.getElementById('banana').value)
-    }
-
-const price ={
-    "apple" : 0.45,
-    "mango" : 0.80,
-    "bannana" : 0.35
-}
-    let totalCost = 0
-    for(let product in basket)
-    {
-         totalCost = totalCost + (basket[product]*price[product])
+function calculateTotal(basket, price) {
+    let totalCost = 0;
+    for (let product in basket) {
+        totalCost += basket[product] * price[product];
     }
     return parseFloat(totalCost.toFixed(2));
 }
 
+document.getElementById('fruitForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
 
-  result = calculateTotal(basket,price)
+    const basket = {
+        apple: parseInt(document.getElementById('apple').value),
+        mango: parseInt(document.getElementById('mango').value),
+        banana: parseInt(document.getElementById('banana').value)
+    };
 
-alert(result)
+    const price = {
+        apple: 0.45,
+        mango: 0.80,
+        banana: 0.35
+    };
+
+    const result = calculateTotal(basket, price);
+    alert(result);
+});
